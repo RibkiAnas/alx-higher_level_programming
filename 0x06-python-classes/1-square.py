efde92539b0b31af25f066db546ed254e3450bf5
Anas Ribki <anasribki147@gmail.com>
2023-06-27 15:20:35 +0100

    Add task 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""define a class Square"""
+
+
+class Square:
+    """This is an empty class that defines a square."""
+    def __init__(self, size):
+        """This method initializes the size attribute
+        of the Square instance."""
+        self.__size = size
