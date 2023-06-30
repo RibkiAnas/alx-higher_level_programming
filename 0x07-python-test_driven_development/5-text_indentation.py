50787940998c3817f67048575e75fafc87dea5ef
Anas Ribki <anasribki147@gmail.com>
2023-06-30 18:07:49 +0100

    Add task 6-max_integer_test.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Define a function text_indentation"""
 
+
 def text_indentation(text):
     """
     Prints a text with 2 new lines after each of these characters: ., ? and :.
