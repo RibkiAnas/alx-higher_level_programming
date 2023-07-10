ef85b1cecdc7c77eb48f7bc056482abb308dfce0
Anas Ribki <anasribki147@gmail.com>
2023-07-10 17:37:37 +0100

    Edit task 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,8 +7,8 @@ Defines inherits_from function
 def inherits_from(obj, a_class):
     """
     Returns True if the object is exactly an
-    instance of a class that inherited 
-    (directly or indirectly) from the 
+    instance of a class that inherited
+    (directly or indirectly) from the
     specified class, otherwise False.
     Args:
         obj: The object to check.
