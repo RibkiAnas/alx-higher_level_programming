4ebb027e09c54e622ca1c858ed8d571b1022eea5
Anas Ribki <anasribki147@gmail.com>
2023-07-11 17:54:27 +0100

    Add task 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Define a Student class
+"""
+
+
+class Student:
+    """
+    Defines a student with first name,
+    last name, and age attributes
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student object with
+        the given first name, last name,
+        and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation
+        of the Student object
+        """
+        if attrs in None:
+            return self.__dict__
+        else:
+            result = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    result[attr] = getattr(self, attr)
+            return result
