5ac21955c8dd89f0f0d4ad44d68431a32793012e
Anas Ribki <anasribki147@gmail.com>
2023-07-11 17:42:52 +0100

    Add task 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Define a class_to_json function
+"""
+
+
+def class_to_json(obj):
+    """
+    Function that returns the dictionary
+    description with simple data structure
+    (list, dictionary, string, integer and
+    boolean) for JSON serialization of an
+    object
+    """
+    return obj.__dict__
