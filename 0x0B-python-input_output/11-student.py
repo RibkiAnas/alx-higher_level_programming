8f5e5f1b90972fb617383eba5838145e5e49f9dc
Anas Ribki <anasribki147@gmail.com>
2023-07-11 19:37:03 +0100

    Add task 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Define a Student class
+"""
+
+
+class Student:
+    """
+    Defines a student with first name,
+    last name, and age attributes
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student object with
+        the given first name, last name,
+        and age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation
+        of the Student object
+        """
+        if isinstance(attrs, list) and all(isinstance(item, str)
+                                           for item in attrs):
+            return {d: getattr(self, d)
+                    for d in attrs
+                    if hasattr(self, d)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student
+        isinstance.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
