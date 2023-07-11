2583eaed3e35f8be2211e54d24b7324b6a17871e
Anas Ribki <anasribki147@gmail.com>
2023-07-11 17:32:06 +0100

    Add task 6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Define a load_from_json_file function
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Function that creates an Object from a
+    “JSON file”
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
