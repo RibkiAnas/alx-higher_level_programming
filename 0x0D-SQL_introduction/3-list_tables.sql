63ad4a6e65f5889948b67d99d56d874727041a01
Anas Ribki <anasribki147@gmail.com>
2023-08-15 21:42:18 +0100

    Edit task 3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,5 +1,2 @@
--- Get the database name from the argument
-SET @dbname = DATABASE();
-
--- Show all tables in the database
-SHOW TABLES FROM @dbname
+-- Show all tables in the database from argument
+SHOW TABLES;
