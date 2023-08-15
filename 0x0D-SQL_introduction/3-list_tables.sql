e8f2a179af6d16e06ffcff8bc1928ca1a96eea54
Anas Ribki <anasribki147@gmail.com>
2023-08-15 21:25:43 +0100

    Edit task 3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,7 +1,5 @@
--- Connect to the database passed as argument
-USE $1;
+-- Get the database name from the argument
+USE $1
 
--- Query the information_schema.tables view to get the table names
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = '$1';
+-- Show all tables in the database
+SHOW TABLES from '$1'
