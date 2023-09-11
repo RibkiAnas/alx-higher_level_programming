dfc1e641e2a7aea89af14cb92725ff68871444c3
Anas Ribki <anasribki147@gmail.com>
2023-09-11 23:14:22 +0100

    Edit task 102-add_me_maybe.js

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 exports.addMeMaybe = function (number, theFunction) {
-  for (let index = 0; index < number; index++) {
-    const incNumber = number++;
-    theFunction(incNumber);
-  }
+    theFunction(number++);
 };
