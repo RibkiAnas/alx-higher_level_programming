d5d7633dc41dd6238aa1218c181a906ced69acf3
Anas Ribki <anasribki147@gmail.com>
2023-09-14 21:03:57 +0100

    Edit port number in task 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     """Connect to the MySQL server"""
     db = MySQLdb.connect(
         host="127.0.0.1",
-        port=3307,
+        port=3306,
         user=username,
         passwd=password,
         db=database,
