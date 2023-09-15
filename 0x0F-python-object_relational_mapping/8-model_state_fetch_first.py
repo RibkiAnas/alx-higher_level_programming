d5b0889fc5d1287362a74f5c0fbdcddb12b054e9
Anas Ribki <anasribki147@gmail.com>
2023-09-15 14:03:57 +0100

    Add task 8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Prints the first State object
+from the database hbtn_0e_6_usa.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+    """Get the arguments from the command line"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    """Create engine that connects to MySQL server at localhost:3306"""
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
+                           .format(username, password, database))
+    Base.metadata.create_all(engine)
+    """Create a configured "Session" class"""
+    Session = sessionmaker(bind=engine)
+    """Create a session"""
+    session = Session()
+    """Query the database"""
+    first_state = session.query(State).first()
+    if first_state is not None:
+        print(first_state.id, first_state.name, sep=": ")
+    else:
+        print("Nothing")
+    """Close the session"""
+    session.close()
