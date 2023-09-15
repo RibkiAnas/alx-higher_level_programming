00fed6d97c283cd10449debaea40579c63e73867
Anas Ribki <anasribki147@gmail.com>
2023-09-15 14:10:06 +0100

    Add task 9-model_state_filter_a.py

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+    """Get the arguments from the command line"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    """Create engine that connects to MySQL server at localhost:3306"""
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
+                           .format(username, password, database))
+    Base.metadata.create_all(engine)
+    """Create a configured "Session" class"""
+    Session = sessionmaker(bind=engine)
+    """Create a session"""
+    session = Session()
+    """Query the database"""
+    for row in session.query(State).order_by(State.id):
+        print(row.id, row.name, sep=", ")
+    """Close the session"""
+    session.close()
