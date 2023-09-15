ce0130ec7af2e03b3aa7c81682b1d26ca0c19697
Anas Ribki <anasribki147@gmail.com>
2023-09-15 16:22:18 +0100

    Add task 16. List relationship

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Lists all State objects, and corresponding City objects, contained
+in the database hbtn_0e_101_usa.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+import sys
+
+
+if __name__ == '__main__':
+    """Get the arguments from the command line"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    """Create engine that connects to MySQL server at localhost:3306"""
+    engine = create_engine("""mysql+mysqldb://{}:{}@localhost:3306/{}"""
+                           .format(username, password, database))
+    Base.metadata.create_all(engine)
+    """Create a configured "Session" class"""
+    Session = sessionmaker(bind=engine)
+    """Create a session"""
+    session = Session()
+    """
+    Query the database for all states, joined with cities,
+    order by states.id and cities.id
+    """
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+    """Close the session"""
+    session.close()
