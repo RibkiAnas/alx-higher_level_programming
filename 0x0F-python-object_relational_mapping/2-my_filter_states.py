a812ac62fd858a29c70a69fea41a7cad2e11b6d1
Anas Ribki <anasribki147@gmail.com>
2023-09-15 12:19:44 +0100

    Add task 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    """Get the arguments from the command line"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    stateName = sys.argv[4]
+    """Connect to the MySQL server"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8")
+    """Create a cursor object"""
+    cursor = db.cursor()
+    """Execute the SQL query"""
+    cursor.execute(f"""SELECT * FROM states
+                   WHERE name = '{stateName}'
+                   ORDER BY states.id ASC;""")
+    """Fetch all the rows from the result set"""
+    rows = cursor.fetchall()
+    """Loop through each row and print it"""
+    for row in rows:
+        print(row)
+    """Close the cursor and the database connection"""
+    cursor.close()
+    db.close()
