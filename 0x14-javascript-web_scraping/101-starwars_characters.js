fc0ee98e9f6bc0c032bb1afac1dbecd463edf8f1
Anas Ribki <anasribki147@gmail.com>
2023-10-24 19:14:59 +0100

    Edit task 8. Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,43 +1,39 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const movieId = process.argv[2];
 
-const baseUrl = 'https://swapi-api.alx-tools.com/api';
-const filmUrl = `${baseUrl}/films/${movieId}`;
+function getCharacterName (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      if (response.statusCode !== 200) {
+        reject(new Error('Invalid status code <' + response.statusCode + '>'));
+      }
+      resolve(JSON.parse(body).name);
+    });
+  });
+}
 
-request.get(filmUrl, (error, res, body) => {
-  if (error) {
-    console.log(error);
-  } else {
+async function printCharacters (movieId) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+  request(url, async (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
     const data = JSON.parse(body);
     const characters = data.characters;
-    let count = characters.length;
-    const names = [];
-
-    for (const character of characters) {
-      request.get(character, (error, res, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          const data = JSON.parse(body);
-
-          names.push(data.name);
-          count--;
-
-          // if count is zero, all character names are fetched
-          if (count === 0) {
-            names.sort(
-              (a, b) => characters.indexOf(`${baseUrl}/people/${a}/`) - characters.indexOf(`${baseUrl}/people/${b}/`)
-            );
-
-            for (const name of names) {
-              console.log(name);
-            }
-          }
-          console.log(data.name);
-        }
-      });
+    for (let i = 0; i < characters.length; i++) {
+      try {
+        const name = await getCharacterName(characters[i]);
+        console.log(name);
+      } catch (error) {
+        console.error('Error: ', error);
+      }
     }
-  }
-});
+  });
+}
+
+printCharacters(movieId);
