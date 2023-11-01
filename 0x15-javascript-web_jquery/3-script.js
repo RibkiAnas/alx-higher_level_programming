2b5b163ea2de5b27cb6bf35eb7e136d8ca8febb3
Anas Ribki <anasribki147@gmail.com>
2023-11-01 20:54:17 +0100

    add task 3. Add  class

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+// script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header
+$(document).ready(() => {
+  $('#red_header').click(() => {
+    $('header').addClass('red');
+  });
+});
